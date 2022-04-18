be292626fcde6c5cd0eb770a1709666d05961596
Wageesha Rasanjana <wageesha.rcc07@gmail.com>
2022-04-19 00:58:01 +0100

    update cuda scripts

---
+++ run_regular_jobs.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+module load system/CUDA/11.0.2-GCC-9.3.0 &&
+make &&
+sbatch cuda_regular_2_5.job &&
+sbatch cuda_regular_2_10.job &&
+sbatch cuda_regular_4_5.job &&
+sbatch cuda_regular_5_10.job &&
+sbatch cuda_regular_8_25.job &&
+sbatch cuda_regular_10_20.job &&
+sbatch cuda_regular_10_25.job &&
+sbatch cuda_regular_10_50.job
