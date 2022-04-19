8e995b81920721a8c240b87b8504deaf993ef7f1
Wageesha Rasanjana <wageesha.rcc07@gmail.com>
2022-04-19 19:07:53 +0100

    update scripts

--- run_regular_jobs.sh
+++ run_regular_jobs.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-module load mpi/OpenMPI/4.0.5-GCC-11.2.0 &&
-make &&
+module load compiler/GCC/11.2.0 mpi/OpenMPI/4.1.1-GCC-11.2.0 && make &&
 sbatch mpi_regular_2.job &&
 sbatch mpi_regular_4.job &&
 sbatch mpi_regular_8.job &&
